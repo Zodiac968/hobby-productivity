fb5dc3ad4414b2c22a3b6f2bcf7a4f1e88893f0d
Mohamed Reihan <mohamedreihan968@gmail.com>
2025-09-05 16:24:41 +0530

    Installed tailwindcss. Added styling to login and registration page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -25,35 +25,59 @@ function Login() {
   };
   return (
     <>
-      <div>
-        <h2>Login</h2>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            ></input>
-          </div>
-          <button type="submit">Login</button>
-          <div>
-            <p>
-              Dont have an account? <Link to="/register">Register</Link>
-            </p>
-          </div>
-        </form>
-        {err && <p>{err}</p>}
+      <div className="flex justify-center items-center h-screen bg-gray-100">
+        <div className="bg-white shadow flex flex-col p-10 pb-5 rounded w-100">
+          <h2 className="text-2xl font-medium text-center m-2">Login</h2>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label className="block font-medium m-1" htmlFor="email">
+                Email
+              </label>
+              <input
+                className="block border border-gray-300 shadow rounded w-5/6 h-10 p-3 m-2"
+                type="email"
+                id="email"
+                placeholder="Enter you email"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                  setErr(null);
+                }}
+                required
+              />
+            </div>
+            <div>
+              <label className="block font-medium m-1" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="block border border-gray-300 shadow rounded w-5/6 h-10 p-3 m-2"
+                type="password"
+                id="password"
+                placeholder="Enter your password"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                  setErr(null);
+                }}
+                required
+              ></input>
+            </div>
+            <button
+              className="bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-800 w-full h-10 text-white rounded-md my-2"
+              type="submit"
+            >
+              Login
+            </button>
+            <div>
+              <p className="text-sm text-center m-1">
+                Dont have an account?{" "}
+                <Link className="text-blue-500 hover:underline" to="/register">
+                  Register
+                </Link>
+              </p>
+            </div>
+          </form>
+          {err && <p className="text-center text-red-500 mt-3">{err}</p>}
+        </div>
       </div>
     </>
   );
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -24,44 +24,66 @@ function Register() {
   };
   return (
     <>
-      <div>
-        <h2>Register</h2>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              id="name"
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            ></input>
-          </div>
-          <button type="submit">Register</button>
-          <div>
-            <p>
-              Go to <Link to="/login">Login</Link>
-            </p>
-          </div>
-        </form>
-        {err && <p>{err}</p>}
+      <div className="flex justify-center items-center h-screen bg-gray-100">
+        <div className="bg-white shadow flex flex-col p-10 pb-5 rounded w-100">
+          <h2 className="text-2xl font-medium text-center m-2">Register</h2>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label className="block font-medium m-1" htmlFor="name">
+                Name
+              </label>
+              <input
+                className="block border border-gray-300 shadow rounded w-5/6 h-10 p-3 m-2"
+                type="text"
+                id="name"
+                placeholder="Enter your name"
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label className="block font-medium m-1" htmlFor="email">
+                Email
+              </label>
+              <input
+                className="block border border-gray-300 shadow rounded w-5/6 h-10 p-3 m-2"
+                type="email"
+                id="email"
+                placeholder="Enter your email"
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label className="block font-medium m-1" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="block border border-gray-300 shadow rounded w-5/6 h-10 p-3 m-2"
+                type="password"
+                id="password"
+                placeholder="Enter your password"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              ></input>
+            </div>
+            <button
+              className="bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-800 w-full h-10 text-white rounded-md my-2"
+              type="submit"
+            >
+              Register
+            </button>
+            <div>
+              <p className="text-sm text-center m-1">
+                Go to{" "}
+                <Link className="text-blue-500 hover:underline" to="/login">
+                  Login
+                </Link>
+              </p>
+            </div>
+          </form>
+          {err && <p className="text-center text-green-500 mt-3">{err}</p>}
+        </div>
       </div>
     </>
   );
